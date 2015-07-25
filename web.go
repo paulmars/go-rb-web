049b3ac9fcc9b0e065f83ee589f1dfa1fd6ebc2b
Paul McKellar <paul.mckellar@gmail.com>
2015-07-25 16:43:14 -0700

    socket

--- web.go
+++ web.go
@@ -1,12 +1,40 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
+  "fmt"
+  "net/http"
+  "os"
+  "code.google.com/p/go.net/websocket"
+  "io"
+  "net/http"
+  "fmt"
+  "log"
 )
 
+func echoHandler(ws *websocket.Conn) {
+
+  receivedtext := make([]byte, 100)
+
+  n,err := ws.Read(receivedtext)
+
+  if err != nil {
+    fmt.Printf("Received: %d bytes\n",n)
+  }
+
+  s := string(receivedtext[:n])
+  fmt.Printf("Received: %d bytes: %s\n",n,s)
+
+  if _, err := ws.Write([]byte("reboot!\n")); err != nil {
+      log.Fatal(err)
+  }
+
+  io.Copy(ws, ws)
+
+  fmt.Printf("Sent: %s\n",s)
+}
+
 func main() {
+  http.Handle("/echo", websocket.Handler(echoHandler))
     http.HandleFunc("/", hello)
     fmt.Println("listening...")
     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
